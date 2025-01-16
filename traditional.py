1f3f6b15d838b02d76a5a92d839fd3b0eaa273f6
Jaanis Fehling <jaanis.fehling@rohde-schwarz.com>
2025-01-16 17:20:04 +0100

    try with traditional approach

---
+++ traditional.py
@@ -0,0 +1,14 @@
+import fitz
+import pandas as pd
+
+fname = "Planwerk JF 28.pdf"
+doc = fitz.open(fname)
+
+result = []
+for i, page in enumerate(doc):
+    tabs = page.find_tables()
+    print(f"Page {i+1} has {len(tabs.tables)} tables")
+    tab = tabs[0]
+    result.append(tab.extract())
+
+print(result)
